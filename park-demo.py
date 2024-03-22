f209b0bb1c7651f86b52de7f9c330cc2e0816011
Sneha-Shajee-Mohan <snehasmohan8@gmail.com>
2024-03-22 16:06:55 -0400

    Update park-demo.py

--- park-demo.py
+++ park-demo.py
@@ -1,13 +1,24 @@
 import streamlit as st
 import pandas as pd
 from b2sdk.v2 import B2Api
+from dotenv import load_dotenv
+from utils.b2 import B2
+import os
+
+load_dotenv()
+
+
+
 
 # Function to fetch data from Backblaze B2
 def fetch_data_from_b2():
     # Initialize B2 API with your account credentials
-    b2 = B2Api()
+    # b2 = B2Api()
     # b2.authorize_account("02d1b3f7b27a", "005a5b8b23b64bdae6bf3279e22b2c6dcc54fb691c")
-    b2.authorize_account('02d1b3f7b27a','005a5b8b23b64bdae6bf3279e22b2c6dcc54fb691c')
+    # b2.authorize_account('02d1b3f7b27a','005a5b8b23b64bdae6bf3279e22b2c6dcc54fb691c')
+    b2 = B2(endpoint=os.environ['B2_ENDPOINT'],
+        key_id=os.environ['B2_KEYID'],
+        secret_key=os.environ['B2_APPKEY'])
 
 
     # Download the CSV file from B2 bucket
