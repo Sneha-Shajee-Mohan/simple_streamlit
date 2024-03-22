29afd5c65e7256a3672a80b28e1ea33b0e78b96d
Sneha-Shajee-Mohan <snehasmohan8@gmail.com>
2024-03-22 16:39:47 -0400

    Update park-demo.py

--- park-demo.py
+++ park-demo.py
@@ -14,7 +14,7 @@ def fetch_data_from_b2():
     # Initialize B2 API with your account credentials
     b2 = B2Api()
     # b2.authorize_account(''005a5b8b23b64bdae6bf3279e22b2c6dcc54fb691c')
-    b2.authorize_account('02d1b3f7b27a','005a5b8b23b64bdae6bf3279e22b2c6dcc54fb691c')
+    b2.authorize_account('005a5b8b23b64bdae6bf3279e22b2c6dcc54fb691c''02d1b3f7b27a',)
                      
     
     # b2.authorize_account(os.environ['B2_KEYID'],os.environ['B2_APPKEY'])
