695e00a5751d133c195b0cf80c9169ddec5a1d77
Sneha-Shajee-Mohan <snehasmohan8@gmail.com>
2024-03-24 20:01:54 -0400

    Update park-demo.py

--- park-demo.py
+++ park-demo.py
@@ -6,7 +6,7 @@ from utils.b2 import B2
 from utils.modeling import *
 import os
 
-REMOTE_DATA = 'https://national-park-demo.s3.us-east-005.backblazeb2.com/NPS.ipynbnational_parks.csv'
+REMOTE_DATA = 'https://f005.backblazeb2.com/file/national-park-demo/NPS.ipynbnational_parks.csv'
 
 load_dotenv()
 
@@ -19,7 +19,7 @@ load_dotenv()
 #         secret_key=os.environ['B2_APPKEY'])
 b2 = B2Api()
 b2.authorize_account("production",'00502d1b3f7b27a0000000001', 'K005Y3TomdGj2uMCd0q8aU7wmuemNQ4')
-# st.write(b2)
+st.write(b2)
 @st.cache_data  
 def get_data():
     # collect data frame of reviews and their sentiment
@@ -29,8 +29,8 @@ def get_data():
     
     # return df
     bucket = b2.get_bucket_by_name(os.environ['B2_BUCKETNAME'])
-    file_info = b2.get_download_url_for_file_name('national-park-demo', 'NPS.ipynbnational_parks.csv')
-    st.write(file_info)
+    # file_info = b2.get_download_url_for_file_name('national-park-demo', 'NPS.ipynbnational_parks.csv')
+    # st.write(file_info)
     # Read the CSV file into a pandas dataframe
     # df = pd.read_csv(file_info)
 
