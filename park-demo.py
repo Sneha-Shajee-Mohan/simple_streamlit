5b5a8df978194023b2835158c1f8b34f134b3942
Sneha-Shajee-Mohan <snehasmohan8@gmail.com>
2024-03-24 20:18:18 -0400

    Update park-demo.py

--- park-demo.py
+++ park-demo.py
@@ -29,7 +29,7 @@ def get_data():
     
     # return df
     bucket = b2.get_bucket_by_name(os.environ['B2_BUCKETNAME'])
-    file_info = b2.get_file_info()
+    file_info = b2.get_file_info('10a2ed515be36f778be2071a')
     # st.write(file_info)
     # Read the CSV file into a pandas dataframe
     df = pd.read_csv(file_info)
