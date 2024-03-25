72ad9fa2c606b619f97fbb281e87fc70e752b878
Sneha-Shajee-Mohan <snehasmohan8@gmail.com>
2024-03-24 20:10:59 -0400

    Update park-demo.py

--- park-demo.py
+++ park-demo.py
@@ -19,7 +19,7 @@ load_dotenv()
 #         secret_key=os.environ['B2_APPKEY'])
 b2 = B2Api()
 b2.authorize_account("production",'00502d1b3f7b27a0000000001', 'K005Y3TomdGj2uMCd0q8aU7wmuemNQ4')
-# st.write(b2)
+st.write(b2)
 @st.cache_data  
 def get_data():
     # collect data frame of reviews and their sentiment
@@ -29,7 +29,7 @@ def get_data():
     
     # return df
     bucket = b2.get_bucket_by_name(os.environ['B2_BUCKETNAME'])
-    file_info = bucket.download_file_by_url(REMOTE_DATA)
+    file_info = bucket.download_file_by_name(REMOTE_DATA)
     # st.write(file_info)
     # Read the CSV file into a pandas dataframe
     df = pd.read_csv(file_info)
